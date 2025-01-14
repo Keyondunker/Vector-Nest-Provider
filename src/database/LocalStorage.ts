3a3216c327e253e2c6836f721946ee178b78f6c8
Muhammed Karamuk <mkaramuk@proton.me>
2025-01-14 11:19:09 +0300

    feat: retrieve provider details from database and cid calculation

--- src/database/LocalStorage.ts
+++ src/database/LocalStorage.ts
@@ -1,14 +1,16 @@
 import { drizzle, NodePgDatabase } from "drizzle-orm/node-postgres";
 import * as schema from "./schema";
 import { config } from "@/config";
-import { NotInitialized } from "@forestprotocols/sdk";
-import { and, desc, eq } from "drizzle-orm";
+import { generateCID, NotInitialized } from "@forestprotocols/sdk";
+import { and, desc, eq, sql } from "drizzle-orm";
 import pg from "pg";
+
+export type DatabaseClientType = NodePgDatabase<typeof schema>;
 
 export class LocalStorage {
   private static _instance: LocalStorage;
 
-  client: NodePgDatabase<typeof schema> | undefined;
+  client: DatabaseClientType | undefined;
 
   private constructor() {}
 
@@ -28,6 +30,102 @@ export class LocalStorage {
     this.client = drizzle(pool, {
       schema,
     });
+  }
+
+  /**
+   * Retrieve all of the details about the provider itself
+   */
+  async getProviderDetails() {
+    this.checkClient();
+    const [result] = await this.client!.select({
+      details: sql<{
+        [key: string]: string;
+      }>`jsonb_object_agg(${schema.providerDetailsTable.name}, ${schema.providerDetailsTable.value})`,
+    }).from(schema.providerDetailsTable);
+
+    if (!result) {
+      return {};
+    }
+
+    if (result.details.cid === undefined) {
+      result.details.cid = (await generateCID(result.details)).toString();
+    }
+
+    return result.details;
+  }
+
+  /**
+   * Adds or updates a provider detail
+   * @param name
+   * @param value
+   * @returns
+   */
+  async setProviderDetail(name: string, value: string) {
+    this.checkClient();
+    return await this.client!.transaction(async (tx) => {
+      const [detail] = await tx
+        .select()
+        .from(schema.providerDetailsTable)
+        .where(eq(schema.providerDetailsTable.name, name));
+
+      if (detail) {
+        // Do we really need to update it?
+        if (detail.value != value) {
+          await tx
+            .update(schema.providerDetailsTable)
+            .set({
+              value,
+            })
+            .where(eq(schema.providerDetailsTable.name, name));
+          return await this.updateProviderDetailsCID(tx);
+        }
+      } else {
+        await tx.insert(schema.providerDetailsTable).values({
+          name,
+          value,
+        });
+        return await this.updateProviderDetailsCID(tx);
+      }
+    });
+  }
+
+  /**
+   * Calculates CID of the provider details
+   * @param tx DB client, can be a TX or client itself.
+   */
+  private async updateProviderDetailsCID(tx: DatabaseClientType) {
+    const [results] = await tx
+      .select({
+        details: sql<{
+          [key: string]: string;
+        }>`jsonb_object_agg(${schema.providerDetailsTable.name}, ${schema.providerDetailsTable.value})`,
+      })
+      .from(schema.providerDetailsTable);
+
+    const details = results?.details || {};
+    const cidExists = details.cid !== undefined;
+
+    // CID itself also stored as a detail but not included in the calculation
+    if (cidExists) {
+      delete details.cid;
+    }
+
+    details.cid = (await generateCID(details)).toString();
+
+    // Save calculated CID to the database
+    if (cidExists) {
+      await tx
+        .update(schema.providerDetailsTable)
+        .set({ value: details.cid })
+        .where(eq(schema.providerDetailsTable.name, "cid"));
+    } else {
+      await tx.insert(schema.providerDetailsTable).values({
+        name: "cid",
+        value: details.cid,
+      });
+    }
+
+    return details;
   }
 
   /**
