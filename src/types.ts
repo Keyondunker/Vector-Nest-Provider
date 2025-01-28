c630ed58d9d6f9e66e77f1faae242d01c667a5de
Muhammed Karamuk <mkaramuk@proton.me>
2025-01-28 11:48:16 +0300

    feat: New type definitions for offers and resources

--- src/types.ts
+++ src/types.ts
@@ -1,4 +1,5 @@
-import { DeploymentStatus } from "@forest-protocols/sdk";
+import { DeploymentStatus, ProviderDetails } from "@forest-protocols/sdk";
+import { Address } from "viem";
 
 /**
  * The base details that should be gathered by
@@ -8,4 +9,31 @@ export type ResourceDetails = {
   status: DeploymentStatus;
   name: string;
   [key: string]: any;
+};
+
+export type OfferDetails = {
+  id: number;
+  details: any;
+  deploymentParams: any;
+  productCategory: Address;
+};
+
+export type Resource = {
+  id: number;
+  name: string;
+  deploymentStatus: DeploymentStatus;
+  details: any;
+  groupName: string;
+  isActive: boolean;
+  ownerAddress: Address;
+  offer: {
+    id: number;
+    details: any;
+    productCategory: Address;
+    provider: {
+      id: number;
+      details: any;
+      ownerAddress: Address;
+    };
+  };
 };
