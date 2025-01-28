58687bd9379abf38d5eee6987ceda497f6fc4ebe
Muhammed Karamuk <mkaramuk@proton.me>
2025-01-28 11:47:48 +0300

    feat: Parse product categories and offers from data files

--- src/config.ts
+++ src/config.ts
@@ -1,8 +1,9 @@
 import { z } from "zod";
 import { red } from "ansis";
-import { readFileSync } from "fs";
+import { readdirSync, readFileSync } from "fs";
 import { join } from "path";
 import {
+  addressSchema,
   getForestRegistryAddress,
   privateKeySchema,
 } from "@forest-protocols/sdk";
@@ -25,6 +26,58 @@ function parseEnv() {
   }
 
   return parsedEnv.data;
+}
+
+function parseProductCategories() {
+  const pcSchema = z.object({
+    address: addressSchema.transform((value) => value.toLowerCase()),
+    name: z.string(),
+    softwareStack: z.string(),
+    version: z.string(),
+    tests: z.array(z.any()), // TODO: Define object shape
+    resourceParams: z.array(
+      z.object({
+        name: z.string(),
+        unit: z.string(),
+        isFilterable: z.boolean(),
+        isPrimary: z.boolean(),
+        priority: z.number().positive(),
+      })
+    ),
+  });
+
+  const productCategories: {
+    [address: string]: z.infer<typeof pcSchema>;
+  } = {};
+
+  try {
+    const basePath = join(process.cwd(), "data/product-categories");
+    const files = readdirSync(basePath, { recursive: true });
+    for (const file of files) {
+      if (!file.toString().endsWith(".json")) {
+        continue;
+      }
+
+      const content = readFileSync(join(basePath, file.toString())).toString();
+      const rawObject = JSON.parse(content);
+      const validation = pcSchema.safeParse(rawObject);
+
+      if (validation.error) {
+        console.error(red(fromError(validation.error)).toString());
+        process.exit(1);
+      }
+
+      const obj = validation.data;
+      productCategories[obj.address] = obj;
+    }
+  } catch (err: any) {
+    console.error(
+      red(`Error while reading product categories: ${err.message}`)
+    );
+    process.exit(1);
+  }
+
+  return productCategories;
 }
 
 function parseProviders() {
@@ -60,18 +113,71 @@ function parseProviders() {
       providers[name] = provider.data!;
     }
   } catch (err: any) {
-    console.error(red("Invalid providers.json file:"), err.message);
+    console.error(red(`Invalid providers.json file: ${err.message}`));
     process.exit(1);
   }
 
   return providers;
 }
 
+function parseOffers() {
+  const offerSchema = z.object({
+    id: z.number(),
+    productCategory: addressSchema.transform((value) => value.toLowerCase()),
+    deploymentParams: z.any(),
+    details: z.object({
+      name: nonEmptyStringSchema,
+      resourceParams: z.array(
+        z.object({
+          name: z.string(),
+          value: z.any(),
+        })
+      ),
+    }),
+  });
+
+  const offers: {
+    [pcAddress: string]: z.infer<typeof offerSchema>[];
+  } = {};
+
+  try {
+    const basePath = join(process.cwd(), "data/offers");
+    const files = readdirSync(basePath, { recursive: true });
+    for (const file of files) {
+      if (!file.toString().endsWith(".json")) {
+        continue;
+      }
+
+      const content = readFileSync(join(basePath, file.toString())).toString();
+      const rawObject = JSON.parse(content);
+      const validation = offerSchema.safeParse(rawObject);
+
+      if (validation.error) {
+        console.error(red(fromError(validation.error)).toString());
+        process.exit(1);
+      }
+
+      const offer = validation.data;
+
+      if (!offers[offer.productCategory]) {
+        offers[offer.productCategory] = [];
+      }
+      offers[offer.productCategory].push(offer);
+    }
+  } catch (err: any) {
+    console.error(red(`Error while reading offers: ${err.message}`));
+    process.exit(1);
+  }
+
+  return offers;
+}
+
 const env = parseEnv();
-const providers = parseProviders();
 
 export const config = {
   ...env,
-  providers,
-  contractAddress: getForestRegistryAddress(env.CHAIN),
+  providers: parseProviders(),
+  productCategories: parseProductCategories(),
+  offers: parseOffers(),
+  registryAddress: getForestRegistryAddress(env.CHAIN),
 };
